fc6ef9c3f229deed358e7f7080ea6e99d6f67e10
sanjeev-db <sanjeev.kumar@databricks.com>
2023-09-02 22:47:13 +0200

    aa

--- job_config/api-call.py
+++ job_config/api-call.py
@@ -16,7 +16,7 @@ data_to_encode = "Hello, World!"
 # Encode the data to Base64
 encoded_data = base64.b64encode(data_to_encode.encode("utf-8")).decode("utf-8")  
 
-payload = "{\n  \"path\": \"/dbfs/FileStore/users/dlt_config1.json\",\n  \"contents\": \"test\",\n  \"overwrite\": \"true\"\n}"
+payload = "{\n  \"path\": \"/dbfs/FileStore/users/dlt_config1.json\",\n  \"contents\": \"teest\",\n  \"overwrite\": \"true\"\n}"
 '''payload = {
     "path": "/dbfs/FileStore/users/dlt-config3.json",
     "contents": "encoded_data",
