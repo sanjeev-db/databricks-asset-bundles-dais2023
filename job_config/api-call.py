87f586de635918c75a3b2a7664336c18ada6c25e
sanjeev-db <sanjeev.kumar@databricks.com>
2023-09-02 23:09:56 +0200

    sss

--- job_config/api-call.py
+++ job_config/api-call.py
@@ -14,7 +14,8 @@ with open("./dlt-config.json", "rb") as file:
 # Data to encode
 data_to_encode = "Hello, World!"  
 # Encode the data to Base64
-encoded_data = base64.b64encode(file_contents.encode("utf-8")).decode("utf-8")  
+#encoded_data = base64.b64encode(file_contents.encode("utf-8")).decode("utf-8")  
+encoded_data = file_contents.decode("utf-8")  
 
 #payload = "{\n  \"path\": \"/dbfs/FileStore/users/dlt_config1.json\",\n  \"contents\": encoded_data,\n  \"overwrite\": \"true\"\n}"
 payload = {
