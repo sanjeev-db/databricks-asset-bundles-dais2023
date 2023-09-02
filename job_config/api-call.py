3de5c591ca04546515cb1376f3cc737fa6947d36
sanjeev-db <sanjeev.kumar@databricks.com>
2023-09-02 21:59:54 +0200

    a

--- job_config/api-call.py
+++ job_config/api-call.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://adb-2701504584282370.10.azuredatabricks.net/api/2.0/dbfs/put"
 
-payload = "{\n  \"path\": \"/dbfs/FileStore/users/dlt_config2.json\",\n  \"contents\": \"test\",\n  \"overwrite\": \"true\"\n}"
+payload = "{\n  \"path\": \"/dbfs/FileStore/users/dlt-config3.json\",\n  \"contents\": \"./dlt-config.json\",\n  \"overwrite\": \"true\"\n}"
 headers = {
   'Content-Type': 'text/plain',
   'Authorization': 'Bearer dapi6fb63c3d7659275e831613df52ac11ac'
