5a9c7266e675bfe90e93212653d1c3ef9d45d27a
sanjeev-db <sanjeev.kumar@databricks.com>
2023-09-02 22:27:27 +0200

    aa

--- job_config/api-call.py
+++ job_config/api-call.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import base64
+
 
 url = "https://adb-2701504584282370.10.azuredatabricks.net/api/2.0/dbfs/put"
 
@@ -9,7 +11,12 @@ url = "https://adb-2701504584282370.10.azuredatabricks.net/api/2.0/dbfs/put"
 with open("./dlt-config.json") as f:
     file_contents = json.load(f)    
 
-payload = "{\n  \"path\": \"/dbfs/FileStore/users/dlt-config3.json\",\n  \"contents\": \"file_contents\",\n  \"overwrite\": \"true\"\n}"
+# Data to encode
+data_to_encode = "Hello, World!"  
+# Encode the data to Base64
+encoded_data = base64.b64encode(data_to_encode.encode("utf-8")).decode("utf-8")  
+
+payload = "{\n  \"path\": \"/dbfs/FileStore/users/dlt-config3.json\",\n  \"contents\": \"encoded_data\",\n  \"overwrite\": \"true\"\n}"
 headers = {
   'Content-Type': 'text/plain',
   'Authorization': 'Bearer dapi6fb63c3d7659275e831613df52ac11ac'
