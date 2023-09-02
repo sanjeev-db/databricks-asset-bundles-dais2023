1393f62015152e92369fc21a9e78dc9c9a1fcd28
sanjeev-db <sanjeev.kumar@databricks.com>
2023-09-02 23:07:44 +0200

    aaa

--- job_config/api-call.py
+++ job_config/api-call.py
@@ -6,10 +6,10 @@ import base64
 url = "https://adb-2701504584282370.10.azuredatabricks.net/api/2.0/dbfs/put"
 
 #Open and read the local file
-#with open("./dlt-config.json", "rb") as file:
-    #file_contents = file.read()
-with open("./dlt-config.json") as f:
-    file_contents = json.load(f)    
+with open("./dlt-config.json", "rb") as file:
+    file_contents = file.read()
+#with open("./dlt-config.json") as f:
+ #   file_contents = json.load(f)    
 
 # Data to encode
 data_to_encode = "Hello, World!"  
