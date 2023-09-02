7bc4afd343c59ca6cf771a2425a82948df25ac95
sanjeev-db <sanjeev.kumar@databricks.com>
2023-09-02 08:16:43 +0200

    a

--- outbound/generic_notebook.py
+++ outbound/generic_notebook.py
@@ -7,7 +7,7 @@ from pyspark.sql.functions import regexp_replace
 
 
 #config_file = "/dbfs/FileStore/users/dlt_config.json"
-config_file = "./dlt_config.json"
+config_file = "/Users/sanjeev.kumar@databricks.com/.bundle/medium_post_report/qa/files/outbound/dlt_config.json"
 
 
 with open(config_file) as f:
