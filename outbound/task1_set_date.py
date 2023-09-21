afb3776a72063d1e7ffd0ba55dbb0b637c8d73e5
sanjeev-db <sanjeev.kumar@databricks.com>
2023-09-21 17:52:27 +0200

    a

--- outbound/task1_set_date.py
+++ outbound/task1_set_date.py
@@ -5,13 +5,9 @@ from datetime import datetime, timedelta
 
 # DBTITLE 1,Get job and task level parameters
 param_job_date = dbutils.widgets.get("run_date")
-param_task_date = dbutils.widgets.get("rundate")
-param_jobid = dbutils.widgets.get("jobid")
+param_jobid = dbutils.widgets.get("job_id")
 param_task2_run_day = int(dbutils.widgets.get("task2_run_day"))
 param_task3_run_day = int(dbutils.widgets.get("task3_run_day"))
-
-print(param_job_date)
-print(param_task_date)
 
 # COMMAND ----------
 
@@ -26,6 +22,7 @@ task3_run_date = date_obj + timedelta(days=param_task3_run_day)
 
 dbutils.jobs.taskValues.set(key="task2_rundate", value=task2_run_date.strftime("%Y-%m-%d"))
 dbutils.jobs.taskValues.set(key="task3_rundate", value=task3_run_date.strftime("%Y-%m-%d"))
+print(task3_run_date)
 
 # COMMAND ----------
 
