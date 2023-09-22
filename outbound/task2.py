4ebb9e282a9b494c0d8a4434eb8c875ab23ac751
sanjeev-db <sanjeev.kumar@databricks.com>
2023-09-22 06:41:50 +0200

    a

--- outbound/task2.py
+++ outbound/task2.py
@@ -1,7 +1,5 @@
 # Databricks notebook source
-rundate = dbutils.widgets.get("rundate")
-
-#rundate = dbutils.jobs.taskValues.get(taskKey="Set_date_for_subsequent_tasks", key="task2_rundate")
+rundate = dbutils.jobs.taskValues.get(taskKey="Set_date_for_subsequent_tasks", key="task2_rundate")
 
 # COMMAND ----------
 
